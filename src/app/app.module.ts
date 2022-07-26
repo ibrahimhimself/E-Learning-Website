c9ea413763dd30b39506baba036374915408031a
ibrahimaboshanab1699@gmail.com <ibrhaimaboshanab1699@gmail.com>
2022-07-26 04:08:02 +0200

    Adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
